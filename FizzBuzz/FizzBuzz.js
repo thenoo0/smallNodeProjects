964c2a2e4b31c9a96fa6f473846790b538c71a90
Mateusz <mateuszlach0@gmail.com>
2025-07-07 20:01:11 +0200

    Add description

--- FizzBuzz/FizzBuzz.js
+++ FizzBuzz/FizzBuzz.js
@@ -1,3 +1,7 @@
+//FizzBuzz is a simple programming challenge that involves printing numbers from 1 to a specified limit (often 100),
+//but replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz", 
+//and multiples of both 3 and 5 with "FizzBuzz".
+
 
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
--- FizzBuzz/FizzBuzz1.js
+++ FizzBuzz/FizzBuzz1.js
@@ -1,19 +1,18 @@
+//FizzBuzz is a simple programming challenge that involves printing numbers from 1 to a specified limit (often 100),
+//but replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz", 
+//and multiples of both 3 and 5 with "FizzBuzz".
+
 const prompt = require('prompt-sync')();
-
 const max = prompt('What is max number you want to check? ');
-
-
 
 for (let i = 1; i <= max; i++) {
     let fizzBuzzWord = '';
     if (i % 3 === 0) {
         fizzBuzzWord = 'Fizz';
     }
-
     if (i % 5 === 0) {
         fizzBuzzWord = fizzBuzzWord + 'Buzz';
     }
-
     if (fizzBuzzWord) {
         console.log(i + ' ' + fizzBuzzWord);
     }
